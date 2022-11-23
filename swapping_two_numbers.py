1543711b48f5f6a0a1c866724e64d2e48eb4527b
mohammed-aasim <118612679+mohammed-aasim@users.noreply.github.com>
2022-11-23 14:21:33 +0530

    getting input from user & swapping it.

---
+++ swapping_two_numbers.py
@@ -0,0 +1,13 @@
+x = input()                      #x kar ko ek variable banate and input lete user kane se
+y = input()
+
+print('The value of x is {}'.format(x))
+print('The value of y is {}'.format(y))
+
+# swapping the values
+temp_var = x
+x = y
+y = temp_var
+
+print('The value of x after swapping is {}'.format(x))
+print('The value of y after swapping is {}'.format(y))
